70c46437d26712b1d34a405ca1e21b1b03b0896f
romis44 <110257777+romis44@users.noreply.github.com>
2023-04-30 18:36:42 +0000

    Deploying to gh-pages from @ romis44/tweets@48bd7d858319381d9a0c446d4cd1099a3fbfba36 🚀

--- static/js/304.d4c774dc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktweets=self.webpackChunktweets||[]).push([[304],{304:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var s="Home_title__qAdt+",c=n(184),u=function(){return(0,c.jsx)("main",{children:(0,c.jsx)("p",{className:s,children:"Welcome to the tweets"})})}}}]);
-//# sourceMappingURL=304.d4c774dc.chunk.js.map
